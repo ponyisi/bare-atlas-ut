0dc0ed1c4c28f2feaafdf6dce4b62488db7ddb57
Kartik Chitturi <kartik.prabhat.chitturi@cern.ch>
2016-12-23 10:48:00 +0000

    Reset hostname explicitly

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+hostname $(curl -s http://utatlas.its.utexas.edu/hostname)
 /etc/cvmfs/setup-cvmfs-portable.sh
 /root/startcondor.sh
 service gmond start
--- startcondor.sh
+++ startcondor.sh
@@ -3,5 +3,6 @@
 echo "TCP_FORWARDING_HOST = $(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)" > /etc/condor/config.d/00-networking
 echo 'PRIVATE_NETWORK_INTERFACE = $(IP_ADDRESS)' >> /etc/condor/config.d/00-networking
 echo "PRIVATE_NETWORK_NAME = nova" >> /etc/condor/config.d/00-networking
+service condor stop
 service condor start
 

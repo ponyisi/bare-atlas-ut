5d0969bc3db8e05d861f4e0e26bb0bb8e3a78672
Kartik Chitturi <kartik.prabhat.chitturi@cern.ch>
2016-12-23 20:21:15 +0000

    New way to check hostname

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-hostname $(curl -s http://utatlas.its.utexas.edu/hostname)
+hostname $(python -c 'import sys, socket; print socket.gethostbyaddr(sys.argv[1])[0]' $(curl -s http://169.254.169.254/latest/meta-data/public-ipv4))
 /etc/cvmfs/setup-cvmfs-portable.sh
 /root/startcondor.sh
 service gmond start

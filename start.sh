8ad50d0f17052d87304f9fc3d76aeba54aea7f58
Kartik Chitturi <kartik.prabhat.chitturi@cern.ch>
2018-02-03 23:06:35 +0000

    Initial working version?

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 hostname $(python -c 'import sys, socket; print socket.gethostbyaddr(sys.argv[1])[0]' $(curl -s http://169.254.169.254/latest/meta-data/public-ipv4))
-/etc/cvmfs/setup-cvmfs-portable.sh
+#/etc/cvmfs/setup-cvmfs-portable.sh
 /root/startcondor.sh
 service gmond start
 exec bash
